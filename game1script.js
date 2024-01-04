85e41fe96b18ba56c9ceef41f2fbd5b6376b7b5e
Justin Riley <20hz20khz@gmail.com>
2024-01-04 14:37:26 -0500

    Update game1script.js

--- game1script.js
+++ game1script.js
@@ -196,7 +196,7 @@ function filterTop(num){
 function filterSpecies(species){
   $('#mymodal').modal('hide');
   questions = unfilteredQuestions.filter(function(each){
-          return each["Tags"].includes("woodpecker");
+          return each["Tags"].includes(species);
   });
   filterImages()
   loadQuestion(randomSeed+currentQuestion);
